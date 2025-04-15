ea3cc820587deff67dfa4e5037b8c6a5d762849a
JJBOY12345 <92385938+JJBOY12345@users.noreply.github.com>
2025-04-15 21:26:23 +0530

    Delete django_backend directory

--- django_backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),  # This maps /api/login/ to our view
-]
